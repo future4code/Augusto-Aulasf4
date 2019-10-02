332f9d32c12038ed039e1209ada1a71f07318155
toguvr <augustotf93@gmail.com>
2019-10-02 16:56:09 -0300

    subindo entrega final

--- semana4/aula21/futuregram/src/App.js
+++ semana4/aula21/futuregram/src/App.js
@@ -10,38 +10,269 @@ class App extends React.Component {
             numeroCurtida:0,
             numeroComentario:0,
             mostraComment:false,
+            imagemSeCurtiu: false,
+            
+            numeroCurtida2:0,
+            numeroComentario2:0,
+            mostraComment2:false,
+            imagemSeCurtiu2: false,
+            
+            numeroCurtida3:0,
+            numeroComentario3:0,
+            mostraComment3:false,
+            imagemSeCurtiu3: false,
         }
+    }
+
+    aumentaComment = () =>{
+        const mostraQntCommentAtual = this.state.numeroComentario
+        const novoComment={
+            numeroComentario: (mostraQntCommentAtual + 1)
+        }
+
+        this.setState(novoComment)
     }
 
     onClickButtonComment = () => {
-        const mostraCommentAtual = this.state.mostraComment;
 
         const abreCaixa = {
-            mostraComment: !mostraCommentAtual
+            mostraComment: true
         }
-      
+        
         this.setState(abreCaixa)
+
+
     }
+
+    alterarQntCurtir = () =>{
+
+        if(this.state.imagemSeCurtiu === true) {
+            const mostraCurtidaAtual = this.state.numeroCurtida;
+            const novaCurtida={
+                numeroCurtida: (mostraCurtidaAtual - 1)
+            }
+    
+            this.setState(novaCurtida)
+    
+          }else{
+            const mostraCurtidaAtual = this.state.numeroCurtida;
+            const novaCurtida={
+                numeroCurtida: (mostraCurtidaAtual + 1)
+            }
+    
+            this.setState(novaCurtida)
+    
+          }
+    }
+
+    alterarIMGCurtir = () => {
+        const imagemSeCurtiuAtual = this.state.imagemSeCurtiu;
+    
+        const novoEstado = {
+          imagemSeCurtiu: !imagemSeCurtiuAtual
+        }
+    
+        this.setState(novoEstado)
+        this.alterarQntCurtir()
+        
+      }
+
+
+
+
+
+
+
+
+
+      aumentaComment2 = () =>{
+        const mostraQntCommentAtual = this.state.numeroComentario2
+        const novoComment={
+            numeroComentario2: (mostraQntCommentAtual + 1)
+        }
+
+        this.setState(novoComment)
+    }
+
+    onClickButtonComment2 = () => {
+
+        const abreCaixa = {
+            mostraComment2: true
+        }
+        
+        this.setState(abreCaixa)
+
+
+    }
+
+    alterarQntCurtir2 = () =>{
+
+        if(this.state.imagemSeCurtiu2 === true) {
+            const mostraCurtidaAtual = this.state.numeroCurtida2;
+            const novaCurtida={
+                numeroCurtida2: (mostraCurtidaAtual - 1)
+            }
+    
+            this.setState(novaCurtida)
+    
+          }else{
+            const mostraCurtidaAtual = this.state.numeroCurtida2;
+            const novaCurtida={
+                numeroCurtida2: (mostraCurtidaAtual + 1)
+            }
+    
+            this.setState(novaCurtida)
+    
+          }
+    }
+
+    alterarIMGCurtir2 = () => {
+        const imagemSeCurtiuAtual = this.state.imagemSeCurtiu2;
+    
+        const novoEstado = {
+          imagemSeCurtiu2: !imagemSeCurtiuAtual
+        }
+    
+        this.setState(novoEstado)
+        this.alterarQntCurtir2()
+        
+      }
+
+
+
+
+
+
+
+      aumentaComment3 = () =>{
+        const mostraQntCommentAtual = this.state.numeroComentario3
+        const novoComment={
+            numeroComentario3: (mostraQntCommentAtual + 1)
+        }
+
+        this.setState(novoComment)
+    }
+
+    onClickButtonComment3 = () => {
+
+        const abreCaixa = {
+            mostraComment3: true
+        }
+        
+        this.setState(abreCaixa)
+
+
+    }
+
+    alterarQntCurtir3 = () =>{
+
+        if(this.state.imagemSeCurtiu3 === true) {
+            const mostraCurtidaAtual = this.state.numeroCurtida3;
+            const novaCurtida={
+                numeroCurtida3: (mostraCurtidaAtual - 1)
+            }
+    
+            this.setState(novaCurtida)
+    
+          }else{
+            const mostraCurtidaAtual = this.state.numeroCurtida3;
+            const novaCurtida={
+                numeroCurtida3: (mostraCurtidaAtual + 1)
+            }
+    
+            this.setState(novaCurtida)
+    
+          }
+    }
+
+    alterarIMGCurtir3 = () => {
+        const imagemSeCurtiuAtual = this.state.imagemSeCurtiu3;
+    
+        const novoEstado = {
+          imagemSeCurtiu3: !imagemSeCurtiuAtual
+        }
+    
+        this.setState(novoEstado)
+        this.alterarQntCurtir3()
+        
+      }
+
+
 
     render(){
         let titulo;
         if(this.state.mostraComment === true){
-            titulo = (<div><input type="text" placeholder="Escreva seu comentário"/><button >Comentar</button></div>)
+            titulo = (<div id="textoComentario"><input type="text" placeholder="Escreva seu comentário"/><button onClick={this.aumentaComment}>Comentar</button></div>)
         }
 
+        let titulo2
+        if(this.state.mostraComment2 === true){
+            titulo = (<div id="textoComentario"><input type="text" placeholder="Escreva seu comentário"/><button onClick={this.aumentaComment2}>Comentar</button></div>)
+        }
+
+        let titulo3;
+        if(this.state.mostraComment3 === true){
+            titulo = (<div id="textoComentario"><input type="text" placeholder="Escreva seu comentário"/><button onClick={this.aumentaComment3}>Comentar</button></div>)
+        }
+
+        let linkDaImagem = require('./icones/favorite.svg');
+        let linkDaImagem2 = require('./icones/favorite.svg');
+        let linkDaImagem3 = require('./icones/favorite.svg');
+
+
+        if(this.state.imagemSeCurtiu === true) {
+            linkDaImagem = require('./icones/favorite.svg');
+          }else{
+            linkDaImagem = require('./icones/favorite-white.svg')
+          }
+
+          if(this.state.imagemSeCurtiu2 === true) {
+            linkDaImagem2 = require('./icones/favorite.svg');
+          }else{
+            linkDaImagem2= require('./icones/favorite-white.svg')
+          }
+
+          if(this.state.imagemSeCurtiu3 === true) {
+            linkDaImagem3 = require('./icones/favorite.svg');
+          }else{
+            linkDaImagem3 = require('./icones/favorite-white.svg')
+          }
+
         const post1 = {
-            imgPessoa: 'https://picsum.photos/200/200',
-            nomePessoa: 'Augusto Telles',
-            imgFeed: 'https://picsum.photos/200/200?e=',
-            imgCurtida: require('./icones/favorite-white.svg'),
+            imgPessoa: 'https://picsum.photos/200/200?e=1',
+            nomePessoa: 'Augusto',
+            imgFeed: 'https://picsum.photos/200/200?e=2',
+            imgCurtida: linkDaImagem,
             numeroCurtida: this.state.numeroCurtida,
             imgComentario: require('./icones/comment_icon.svg'),
             numeroComentario: this.state.numeroComentario
         }
-    
+
+        const post2 = {
+            imgPessoa: 'https://picsum.photos/200/200?e=3',
+            nomePessoa: 'Darvas',
+            imgFeed: 'https://picsum.photos/200/200?e=4',
+            imgCurtida: linkDaImagem2,
+            numeroCurtida: this.state.numeroCurtida2,
+            imgComentario: require('./icones/comment_icon.svg'),
+            numeroComentario: this.state.numeroComentario2
+        }
+
+        const post3 = {
+            imgPessoa: 'https://picsum.photos/200/200?e=5',
+            nomePessoa: 'Soter',
+            imgFeed: 'https://picsum.photos/200/200?e=6',
+            imgCurtida: linkDaImagem3,
+            numeroCurtida: this.state.numeroCurtida,
+            imgComentario: require('./icones/comment_icon.svg'),
+            numeroComentario: this.state.numeroComentario
+        }
+
         return (
             <div className="App">
                 <CaixaInsta 
+                    apareceCaixaComment = {this.onClickButtonComment}
+                    curtir = {this.alterarIMGCurtir}
                     imgPessoa = {post1.imgPessoa}
                     nomePessoa = {post1.nomePessoa}
                     imgFeed = {post1.imgFeed}
@@ -49,12 +280,32 @@ class App extends React.Component {
                     numeroCurtida = {post1.numeroCurtida}
                     imgComentario = {post1.imgComentario}
                     numeroComentario = {post1.numeroComentario}
-                >
-                
-                    {titulo}
-                
-                </CaixaInsta>
-
+                    caixadecomentario={titulo}
+                />
+                <CaixaInsta 
+                    apareceCaixaComment = {this.onClickButtonComment2}
+                    curtir = {this.alterarIMGCurtir2}
+                    imgPessoa = {post2.imgPessoa}
+                    nomePessoa = {post2.nomePessoa}
+                    imgFeed = {post2.imgFeed}
+                    imgCurtida = {post2.imgCurtida}
+                    numeroCurtida = {post2.numeroCurtida}
+                    imgComentario = {post2.imgComentario}
+                    numeroComentario = {post2.numeroComentario}
+                    caixadecomentario={titulo2}
+                />
+                <CaixaInsta 
+                    apareceCaixaComment = {this.onClickButtonComment3}
+                    curtir = {this.alterarIMGCurtir3}
+                    imgPessoa = {post3.imgPessoa}
+                    nomePessoa = {post3.nomePessoa}
+                    imgFeed = {post3.imgFeed}
+                    imgCurtida = {post3.imgCurtida}
+                    numeroCurtida = {post3.numeroCurtida}
+                    imgComentario = {post3.imgComentario}
+                    numeroComentario = {post3.numeroComentario}
+                    caixadecomentario={titulo3}
+                />
 
             </div>
         );
--- semana4/aula21/futuregram/src/Components/CaixaInsta.js
+++ semana4/aula21/futuregram/src/Components/CaixaInsta.js
@@ -12,20 +12,24 @@ export function CaixaInsta(props){
             <img className="img-post" src={props.imgFeed} alt="imagem do feed"/>
             <footer>
                 <div className="curtida">
-                    <img src={props.imgCurtida} alt="curtidas"/>
+                    <img onClick={props.curtir} src={props.imgCurtida} alt="curtidas"/>
                     <p>{props.numeroCurtida}</p>
                 </div>
                 <div className="comentario">
                     <p>{props.numeroComentario}</p>
-                    <img  src={props.imgComentario} alt="comentarios"/>
+                    <img onClick={props.apareceCaixaComment} src={props.imgComentario} alt="comentarios"/>
                 </div>
             </footer>
+            {props.caixadecomentario}
         </main>
     )
 }
 
 CaixaInsta.propTypes = {
+    apareceCaixaComment: PropTypes.func.isRequired,
+    curtir: PropTypes.func.isRequired,
     imgPessoa: PropTypes.string.isRequired,
+    caixadecomentario: PropTypes.object,
     nomePessoa: PropTypes.string.isRequired,
     imgFeed: PropTypes.string.isRequired,
     imgCurtida: PropTypes.string.isRequired,
