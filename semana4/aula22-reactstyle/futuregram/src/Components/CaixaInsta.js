9f61482b54f4a53bd4ddfc5d3353b9f97efbd66b
toguvr <augustotf93@gmail.com>
2019-10-03 23:34:04 -0300

    subindo ja com as tarefas finalizadas, faltou apenas passar para o component-styled tb o component ContainerInsta, e ja com o primeiro do extra concluido sem querer kkk

--- semana4/aula22-reactstyle/futuregram/src/Components/CaixaInsta.js
+++ semana4/aula22-reactstyle/futuregram/src/Components/CaixaInsta.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './CaixaInsta.css'
-import styled from 'styled-components';
+// import styled from 'styled-components';
 
 
 export class CaixaInsta extends React.Component{
