5a9f969d455994085bb01ca061111f9fc268a92f
toguvr <augustotf93@gmail.com>
2019-11-22 16:48:13 -0300

    subindo testes

--- semana10/src/containers/Planner/index.test.js
+++ semana10/src/containers/Planner/index.test.js
@@ -28,29 +28,14 @@ describe("Planner Component", () => {
         const includeButton = component.find(IncludeButton);
 
         expect(includeButton).toHaveLength(1);
+        
+    })  
 
-
-        test("IncludeButton", () => {
-
-            const getTask = jest.fn()
-            const createTask = jest.fn()
-    
-            const component = shallow(
-                <Planner allTasks={[mockTask]} getTasks={getTask} createTask={createTask}/>
-              );
-    
-              const instance = component.instance();
-    
-              instance.includeActivity = jest.fn()
-              component.update()
-    
-            const includeButton = component.find(IncludeButton);
-    
-            expect(includeButton).toHaveLength(1);
-    
-            includeButton.simulate("click");
-    
-            expect(instance.includeActivity).toHaveBeenCalled()
-        })
-    })
+    test("IncludeButton", () => {
+        
+        const getTask = jest.fn()
+        const createTask = jest.fn()
+          const wrapper = shallow(<Planner allTasks={[mockTask]} getTasks={getTask} createTask={createTask}/>);
+          expect(wrapper.find(IncludeButton).children()).toHaveLength(1);
+    })  
 })
